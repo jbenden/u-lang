cfe2090b9ba80793f25b5eb2394cc14fb1e3f208
Joseph Benden <joe@benden.us>
2018-04-19 12:00:19 -0700

    Fixed file license formatting.

--- include/u-lang/u.hpp
+++ include/u-lang/u.hpp
@@ -1,6 +1,7 @@
 /**
  * The U Programming Language
- * Copyright 2018 Joseph Benden <joe@benden.us>
+ *
+ * Copyright 2018 Joseph Benden
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
