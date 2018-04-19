cb30902ad6b362e710cacaf6aa5eff59ceb55b26
Joseph Benden <joe@benden.us>
2018-04-19 12:19:46 -0700

    Initial commit for testing CI sanity.

---
+++ include/u-lang/Basic/Source.hpp
@@ -0,0 +1,39 @@
+/**
+ * The U Programming Language
+ *
+ * Copyright 2018 Joseph Benden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * \author Joseph W. Benden
+ * \copyright (C) 2018 Joseph Benden
+ * \license apache2
+ */
+
+#ifndef U_LANG_SOURCE_HPP
+#define U_LANG_SOURCE_HPP
+
+#include <u-lang/u.hpp>
+
+namespace u
+{
+
+class UAPI Source
+{
+public:
+  Source() = default;
+};
+
+} /* namespace u */
+
+#endif // U_LANG_SOURCE_HPP
---
+++ tests/Basic/Source.cpp
@@ -0,0 +1,34 @@
+/**
+ * The U Programming Language
+ *
+ * Copyright 2018 Joseph Benden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * \author Joseph W. Benden
+ * \copyright (C) 2018 Joseph Benden
+ * \license apache2
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <u-lang/u.hpp>
+#include <u-lang/Basic/Source.hpp>
+
+using namespace u;
+
+TEST(BasicSource, CanPassSanity) // NOLINT
+{
+  EXPECT_TRUE(true);
+}
