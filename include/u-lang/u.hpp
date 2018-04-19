8423c3bc4d5e986e70e6334638474434612c7630
Joseph Benden <joe@benden.us>
2018-04-19 11:48:01 -0700

    Fixed file license.

--- include/u-lang/u.hpp
+++ include/u-lang/u.hpp
@@ -1,14 +1,22 @@
 /**
  * The U Programming Language
- * (C) 2018 Joseph Benden <joe@benden.us>
+ * Copyright 2018 Joseph Benden <joe@benden.us>
  *
- * Licensed under the terms of the LGPL v2 license. For more information please
- * see the LICENSE file in the root of the project's directory or located in
- * the doc directory.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * \author Joseph W. Benden
  * \copyright (C) 2018 Joseph Benden
- * \license lgpl
+ * \license apache2
  */
 
 #ifndef ULANG_U_H
