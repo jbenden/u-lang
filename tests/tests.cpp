609d6e1d6cc767aa6bf1c3b6f42dfd85a8b4e0d3
Joseph Benden <joe@benden.us>
2018-04-20 09:39:56 -0700

    Added Google Logging to unit-tests.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -22,10 +22,14 @@
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <glog/logging.h>
 
 int
 main(int argc, char **argv)
 {
+  ::google::InitGoogleLogging(argv[0]);
+
   ::testing::InitGoogleMock(&argc, argv);
+
   return RUN_ALL_TESTS();
 }
